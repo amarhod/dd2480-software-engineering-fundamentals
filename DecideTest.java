2188863757cbffd7b3c7c69d709c682f2a6083bc
GustavDowling <47000821+GustavDowling@users.noreply.github.com>
2021-01-27 19:20:17 +0100

    Code skeleton for testing
    Boilerplate asserts in case this is the route we go for for testing

---
+++ DecideTest.java
@@ -0,0 +1,17 @@
+
+
+class DecideTest {
+	// File containing test cases for Decide.java
+    
+    public static void main(String[] args) {
+		//To run test cases, need to add "-ea" option:
+		//"java -ea DecideTest"
+		
+		//assert case that always succeeds
+		assert 2+2 == 4 : " Should not fail, true";
+		//assert case that always fails
+		assert 3+3 == 5 : " should fail, showing how it looks";
+		
+
+	}
+}
