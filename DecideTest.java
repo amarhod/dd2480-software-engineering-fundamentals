8f3c08414f0009ac1bb8cc9ec4b1cf6d7ed77563
Amar Hodzic <amar_hodzic@hotmail.com>
2021-01-28 14:40:45 +0100

    feat: Added test for LIC0

--- DecideTest.java
+++ DecideTest.java
@@ -6,12 +6,8 @@ class DecideTest {
     public static void main(String[] args) {
 		//To run test cases, need to add "-ea" option:
 		//"java -ea DecideTest"
-		
-		//assert case that always succeeds
-		assert 2+2 == 4 : " Should not fail, true";
-		//assert case that always fails
-		assert 3+3 == 5 : " should fail, showing how it looks";
-		
 
+		Decide decide = new Decide();
+		assert true == decide.LIC0() : "LIC0 Failed";
 	}
 }
