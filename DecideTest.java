1e494c7f43a451893b214ee3031dfa5b883dfce6
Amar Hodzic <amar_hodzic@hotmail.com>
2021-01-29 11:17:15 +0100

    refactor: Rewrote test for LIC0

--- DecideTest.java
+++ DecideTest.java
@@ -8,6 +8,11 @@ class DecideTest {
 		//"java -ea DecideTest"
 
 		Decide decide = new Decide();
-		assert true == decide.LIC0() : "LIC0 Failed";
+		double[] X1 = {0.23, 6.51};
+		double[] Y1 = {1.31, 2.20};
+		assert true == decide.LIC0(X1, Y1, 2, 4.4) : "LIC0 (true) Failed";
+		double[] X2 = {0.23, 1.51};
+		double[] Y2 = {1.31, 2.20};
+		assert false == decide.LIC0(X2, Y2, 2, 1.56) : "LIC0 (false) Failed";				
 	}
 }
