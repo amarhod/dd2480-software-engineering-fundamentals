768fe221ee5468ee7b31b7c681aa162a10e600a8
Amar Hodzic <amar_hodzic@hotmail.com>
2021-01-31 12:55:59 +0100

    feat: Added test for LIC1

--- DecideTest.java
+++ DecideTest.java
@@ -15,11 +15,18 @@ class DecideTest {
 		double[] Y2 = {1.31, 2.20};
 		assert false == decide.LIC0(X2, Y2, 2, 1.56) : "LIC0 (false) Failed";	
 
+		double[] LIC1_X1 = {-1.0, 0.0, 1.0};
+		double[] LIC1_Y1 = {0, 0, 0};
+		assert true == decide.LIC1(LIC1_X1, LIC1_Y1, 3, 0.95) : "LIC1 (true) Failed";
+		double[] LIC1_X2 = {-1.0, 0.0, 1.0};
+		double[] LIC1_Y2 = {0, 0, 0};
+		assert false == decide.LIC1(LIC1_X2, LIC1_Y2, 3, 1.0) : "LIC1 (false) Failed";	
+
 		double[] LIC4_X1 = {0, -1, 0, 0 ,1, 1};
 		double[] LIC4_Y1 = {0, 0, -1, 1, 0,-1};
-		assert true == decide.LIC4(LIC4_X1, LIC4_Y1, 5, 3) : "LIC0 (true) Failed";
+		assert true == decide.LIC4(LIC4_X1, LIC4_Y1, 5, 3) : "LIC4 (true) Failed";
 		double[] LIC4_X2 = {0, 1, 0, 2 ,1, 1};
 		double[] LIC4_Y2 = {0, 0, -1, 2, 0,-1};
-		assert false == decide.LIC4(LIC4_X2, LIC4_Y2, 5, 3) : "LIC0 (false) Failed";			
+		assert false == decide.LIC4(LIC4_X2, LIC4_Y2, 5, 3) : "LIC4 (false) Failed";			
 	}
 }
