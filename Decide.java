f88b3b3aae6613fb30340ea690101b87c1af6fa6
gdowling <gdowling@kth.se>
2021-02-04 13:05:48 +0100

    ref: Seperated matrix calculations into seperate file MatrixCalc.java #7 fix: Unit tests for LIC's #9

--- Decide.java
+++ Decide.java
@@ -3,98 +3,19 @@
 class Decide {
     //Program for deciding hypothetical anti-ballistic missile system
     //Call method decide with appropriate input to decide to send an interceptor or not.
-
-    //conditions met vector
-    public boolean[] CMV(double[] X, double[] Y, int numpoints, double length1, double length2, double area1, double area2,
-                            double radius1, double radius2, double epsilon, double dist, int a_pts, int b_pts, int c_pts, 
-                            int d_pts, int e_pts, int f_pts, int g_pts, int k_pts, int n_pts, int q_pts, int quads) {
-        //computes the conditions met vector from the LIC's
-		boolean[] vector = new boolean[15];
-		LIC lic = new LIC();
-        vector[0] = lic.LIC0(X, Y, numpoints, length1);
-        vector[1] = lic.LIC1(X, Y, numpoints, radius1);
-        vector[2] = lic.LIC2(X, Y, numpoints, epsilon);
-        vector[3] = lic.LIC3(X, Y, numpoints, area1);
-        vector[4] = lic.LIC4(X, Y, numpoints, q_pts, quads);
-        vector[5] = lic.LIC5(X, Y, numpoints);
-        vector[6] = lic.LIC6(X, Y, numpoints, n_pts, dist);
-        vector[7] = lic.LIC7(X, Y, numpoints, length1, k_pts);
-        vector[8] = lic.LIC8(X, Y, numpoints, radius1, a_pts, b_pts);
-        vector[9] = lic.LIC9(X, Y, numpoints, epsilon, c_pts, d_pts);
-        vector[10] = lic.LIC10(X, Y, numpoints, area1, e_pts, f_pts);
-        vector[11] = lic.LIC11(X, Y, numpoints, g_pts);
-        vector[12] = lic.LIC12(X, Y, numpoints, length1, length2, k_pts);
-        vector[13] = lic.LIC13(X, Y, numpoints, radius1, radius2, a_pts, b_pts);
-        vector[14] = lic.LIC14(X, Y, numpoints, area1, area2, e_pts, f_pts);
-        return vector;
-    }
-
-    //Preliminary unlock matrix
-    public boolean[][] PUM(boolean[] cmv, String[][] lcm) {
-		//computes the PUM matrix from the CMV and the LCM
-		
-		boolean matrix[][] = new boolean[15][15];
-		
-		for (int i=0; i < 15; i++){
-			for (int j=0; j < 15; j++){
-				if (lcm[i][j] == "ANDD") {
-					if (cmv[i] && cmv[j]){
-						matrix[i][j] = true;
-					} else {
-						matrix[i][j] = false;
-					}
-				} else if (lcm[i][j] == "ORR") {
-					if (cmv[i] || cmv[j]){
-						matrix[i][j] = true;
-					} else {
-						matrix[i][j] = false;
-					}
-				} else if (lcm[i][j] == "NOTUSED") {
-					matrix[i][j] = true;
-				}
-			}
-		}
-		
-        return matrix;
-	}
-	
-	//Final unlock vector
-	public boolean[] FUV(boolean[][] pum, boolean[] puv) {
-		//computes the FUV vector from the PUM and the PUV
-		
-		boolean vector[] = new boolean[15];
-		boolean allTrueFlag = true; //set to false if any element in row i of the PUM is false
-		
-		for (int i=0; i < 15; i++){
-			allTrueFlag = true;
-			if (puv[i] == false) {
-				vector[i] = true;
-			} else {
-				for (int j=0; j < 15; j++){
-					if (pum[i][j] == false){
-						allTrueFlag = false; //all elements in row are not true
-					}
-				}
-				if (allTrueFlag) {
-					vector[i] = true;
-				} else {
-					vector[i] = false;
-				}
-			}
-		}
-		
-        return vector;
-	}
     
     //decide method
     public boolean decide(double[] X, double[] Y, int numpoints, double length1, double length2, double area1, double area2,
 						double radius1, double radius2, double epsilon, double dist, int a_pts, int b_pts, int c_pts, 
 						int d_pts, int e_pts, int f_pts, int g_pts, int k_pts, int n_pts, int q_pts, int quads, String[][] LCM, boolean[] PUV) {
 						//decides method, calls CMV, PUM, FUV and decides if it launches or not
-		boolean[] cmv = CMV(X, Y, numpoints, length1, length2, area1, area2, radius1, radius2, epsilon, 
+						
+		MatrixCalc matrixcalc = new MatrixCalc();
+		
+		boolean[] cmv = matrixcalc.CMV(X, Y, numpoints, length1, length2, area1, area2, radius1, radius2, epsilon, 
 		dist, a_pts, b_pts, c_pts, d_pts, e_pts, f_pts, g_pts, k_pts, n_pts, q_pts, quads);
-		boolean[][] pum = PUM(cmv, LCM);
-		boolean[] fuv = FUV(pum, PUV);
+		boolean[][] pum = matrixcalc.PUM(cmv, LCM);
+		boolean[] fuv = matrixcalc.FUV(pum, PUV);
 		boolean launch = true;
 		for(boolean v: fuv){
 			if(!v){launch = false;}
--- DecideTest.java
+++ DecideTest.java
@@ -1,7 +1,7 @@
 class DecideTest {
 	// File containing test cases for Decide.java
 	
-	public static viod LIC0_test() {
+	public static void LIC0_test() {
 	    LIC lic = new LIC();
 		double[] X1 = {0.23, 6.51};
 		double[] Y1 = {1.31, 2.20};
@@ -12,7 +12,7 @@ class DecideTest {
 	
 	}
     
-	public static viod LIC1_test() {
+	public static void LIC1_test() {
 	    LIC lic = new LIC();
 		double[] LIC1_X1 = {-1.0, 0.0, 1};
 		double[] LIC1_Y1 = {0, 0, 0};
@@ -23,7 +23,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC2_test() {
+	public static void LIC2_test() {
 	    LIC lic = new LIC();
 		//angle less than pi - epsilon = 90 degrees
 		double[] LIC2_X1 = {0, 1, 0.5};
@@ -40,7 +40,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC3_test() {
+	public static void LIC3_test() {
 	    LIC lic = new LIC();
 		double[] LIC3_X1 = {0, 0, 0, 0 ,3, 0};
 		double[] LIC3_Y1 = {0, 0, 0, 1, 0, 6};
@@ -51,7 +51,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC4_test() {
+	public static void LIC4_test() {
 	    LIC lic = new LIC();
 		double[] LIC4_X1 = {0, -1, 0, 0 ,1, 1};
 		double[] LIC4_Y1 = {0, 0, -1, 1, 0,-1};
@@ -62,7 +62,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC5_test() {
+	public static void LIC5_test() {
 	    LIC lic = new LIC();
 		double[] LIC5_X1 = {0, -1, 0};
 		double[] LIC5_Y1 = {0, 0, 0};
@@ -73,7 +73,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC6_test() {
+	public static void LIC6_test() {
 	    LIC lic = new LIC();
 		double[] LIC6_X1 = {0, -1, 0, 0 ,1, 0};
 		double[] LIC6_Y1 = {0, 0, -1, 0, 0,-1};
@@ -84,7 +84,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC7_test() {
+	public static void LIC7_test() {
 	    LIC lic = new LIC();
 		//k_pts = 1, length1 = 10
 		double[] LIC7_X1 = {0, -1, 0, 15 ,1, 0};
@@ -96,7 +96,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC8_test() {
+	public static void LIC8_test() {
 	    LIC lic = new LIC();
 		double[] LIC8_X1 = {-1.0, 0 ,0, 0, 1.0, 0, 0};
 		double[] LIC8_Y1 = {0, 0, 0, 0, 0, 0, 0};
@@ -107,7 +107,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC9_test() {
+	public static void LIC9_test() {
 	    LIC lic = new LIC();
 		double[] LIC9_X1 = {0, 0, 0, 0, 1.0};
 		double[] LIC9_Y1 = {0, 0, 1.0, 0, 1.0};
@@ -118,7 +118,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC10_test() {
+	public static void LIC10_test() {
 	    LIC lic = new LIC();
 		double[] LIC10_X1 = {0, 0, 1.0, 0, 2.0};
 		double[] LIC10_Y1 = {0, 0, 2.0, 0, 0};
@@ -129,7 +129,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC11_test() {
+	public static void LIC11_test() {
 	    LIC lic = new LIC();
 		double[] LIC11_X1 = {0, 1, 2, 3 ,-1, 0};
 		double[] LIC11_Y1 = {0, 0, -1, 0, 0,-1};
@@ -140,7 +140,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC12_test() {
+	public static void LIC12_test() {
 	    LIC lic = new LIC();
 		//k_pts = 2, length1 = 5, length2 = 100
 		double[] LIC12_X1 = {0, 1, 0, 0, 10};
@@ -157,7 +157,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC13_test() {
+	public static void LIC13_test() {
 	    LIC lic = new LIC();
 		//a_pts = 1, b_pts = 1, radius1 = 10 radius2 = 100
 		double[] LIC13_X1 = {0, 1, 0, 0, 10};
@@ -169,7 +169,7 @@ class DecideTest {
 	
 	}
 	
-	public static viod LIC14_test() {
+	public static void LIC14_test() {
 	    LIC lic = new LIC();
 		//e_pts = 1, f_pts = 1, area1 = 5 area2 = 100
 		double[] LIC14_X1 = {0, 0, 4, 0, 0};
@@ -180,7 +180,6 @@ class DecideTest {
 		assert false == lic.LIC14(LIC14_X2, LIC14_Y2, 5, 5, 100, 1, 1) : "LIC14 (false) Failed";
 	
 	}
-	
 	
     public static void main(String[] args) {
 		//To run test cases, need to add "-ea" option:
@@ -196,159 +195,27 @@ class DecideTest {
 		smallest = lic.smallestRadius(0,0,1,2,2,0);
 		assert ( smallest <= 1.26 && smallest >= 1.24) : "smallest radius not within expected range for acute case";
 		
-		//LIC0
 		LIC0_test();
-		//~double[] X1 = {0.23, 6.51};
-		//~double[] Y1 = {1.31, 2.20};
-		//~assert true == lic.LIC0(X1, Y1, 2, 4.4) : "LIC0 (true) Failed";
-		//~double[] X2 = {0.23, 1.51};
-		//~double[] Y2 = {1.31, 2.20};
-		//~assert false == lic.LIC0(X2, Y2, 2, 1.56) : "LIC0 (false) Failed";	
-		
-		//LIC1
 		LIC1_test();
-		//~double[] LIC1_X1 = {-1.0, 0.0, 1};
-		//~double[] LIC1_Y1 = {0, 0, 0};
-		//~assert true == lic.LIC1(LIC1_X1, LIC1_Y1, 3, 0.95) : "LIC1 (true) Failed";
-		//~double[] LIC1_X2 = {-1.0, 0.0, 1.0};
-		//~double[] LIC1_Y2 = {0, 0, 0};
-		//~assert false == lic.LIC1(LIC1_X2, LIC1_Y2, 3, 1.0) : "LIC1 (false) Failed";	
-		
-		//LIC2
 		LIC2_test();
-		//angle less than pi - epsilon = 90 degrees
-		//~double[] LIC2_X1 = {0, 1, 0.5};
-		//~double[] LIC2_Y1 = {0, 0, 1};
-		//~assert true == lic.LIC2(LIC2_X1, LIC2_Y1, 3, Math.PI/2) : "LIC2 (true, angle < 90) Failed";
-		//angle more than pi + epsilon = 270 degrees
-		//~double[] LIC2_X2 = {0, 1, 0.5};
-		//~double[] LIC2_Y2 = {0, 0, -1};
-		//~assert true == lic.LIC2(LIC2_X2, LIC2_Y2, 3, Math.PI/2) : "LIC2 (true, angle > 270) Failed";
-		//angles between 90 and 270
-		//~double[] LIC2_X3 = {0, 1, 2, 3};
-		//~double[] LIC2_Y3 = {0, 0, 2, 2};
-		//~assert false == lic.LIC2(LIC2_X3, LIC2_Y3, 4, Math.PI/2) : "LIC2 (false) Failed";
-		
-		//LIC3
 		LIC3_test();
-		//~double[] LIC3_X1 = {0, 0, 0, 0 ,3, 0};
-		//~double[] LIC3_Y1 = {0, 0, 0, 1, 0, 6};
-		//~assert true == lic.LIC3(LIC3_X1, LIC3_Y1, 6, 3) : "LIC3 (true) Failed";
-		//~double[] LIC3_X2 = {0, -1, 0, 0 ,1, 1};
-		//~double[] LIC3_Y2 = {0, 0, -1, 1, 0,-1};
-		//~assert false == lic.LIC3(LIC3_X2, LIC3_Y2, 6, 3) : "LIC3 (false) Failed";
-		
-		//LIC4
 		LIC4_test();
-		//~double[] LIC4_X1 = {0, -1, 0, 0 ,1, 1};
-		//~double[] LIC4_Y1 = {0, 0, -1, 1, 0,-1};
-		//~assert true == lic.LIC4(LIC4_X1, LIC4_Y1, 6, 5, 3) : "LIC4 (true) Failed";
-		//~double[] LIC4_X2 = {0, 1, 0, 2 ,1, 1};
-		//~double[] LIC4_Y2 = {0, 0, -1, 2, 0,-1};
-		//~assert false == lic.LIC4(LIC4_X2, LIC4_Y2, 6, 5, 3) : "LIC4 (false) Failed";
-	    
-	    //LIC5
 		LIC5_test();
-		//~double[] LIC5_X1 = {0, -1, 0};
-		//~double[] LIC5_Y1 = {0, 0, 0};
-		//~assert true == lic.LIC5(LIC5_X1, LIC5_Y1, 3) : "LIC5 (true) Failed";
-		//~double[] LIC5_X2 = {0, 1, 2};
-		//~double[] LIC5_Y2 = {0, 0, 0};
-		//~assert false == lic.LIC5(LIC5_X2, LIC5_Y2, 3) : "LIC5 (false) Failed";	
-		
-		//LIC6
 		LIC6_test();
-		//~double[] LIC6_X1 = {0, -1, 0, 0 ,1, 0};
-		//~double[] LIC6_Y1 = {0, 0, -1, 0, 0,-1};
-		//~assert true == lic.LIC6(LIC6_X1, LIC6_Y1, 6, 4, 1.25) : "LIC6 (true) Failed";
-		//~double[] LIC6_X2 = {0, 0, 0, 0 ,1, 5};
-		//~double[] LIC6_Y2 = {0, 0, -2, 0, 0, 5};
-		//~assert false == lic.LIC6(LIC6_X2, LIC6_Y2, 6, 4, 2) : "LIC6 (false) Failed";
-		
-		//LIC7
 		LIC7_test();
-		//k_pts = 1, length1 = 10
-		//~double[] LIC7_X1 = {0, -1, 0, 15 ,1, 0};
-		//~double[] LIC7_Y1 = {0, 0, -1, 0, 0,-1};
-		//~assert true == lic.LIC7(LIC7_X1, LIC7_Y1, 6, 10, 1) : "LIC7 (true) Failed";
-		//~double[] LIC7_X2 = {0, -1, 0, 8 ,1, 0};
-		//~double[] LIC7_Y2 = {0, 0, -1, 0, 0,-1};
-		//~assert false == lic.LIC7(LIC7_X2, LIC7_Y2, 6, 10, 1) : "LIC7 (false) Failed";
-		
-		
-		//LIC8
 		LIC8_test();
-		//~double[] LIC8_X1 = {-1.0, 0 ,0, 0, 1.0, 0, 0};
-		//~double[] LIC8_Y1 = {0, 0, 0, 0, 0, 0, 0};
-		//~assert true == lic.LIC8(LIC8_X1, LIC8_Y1, 7, 0.95, 1, 1) : "LIC8 (true) Failed";
-		//~double[] LIC8_X2 = {-1.0, 1.5, 0, 0, 1.0, -0.5, 0};
-		//~double[] LIC8_Y2 = {0, 0, 0, 0, 0, 0, 0};
-		//~assert false == lic.LIC8(LIC8_X2, LIC8_Y2, 7, 1.0, 1, 1) : "LIC8 (false) Failed";
-		
-		//LIC9
 		LIC9_test();
-		//~double[] LIC9_X1 = {0, 0, 0, 0, 1.0};
-		//~double[] LIC9_Y1 = {0, 0, 1.0, 0, 1.0};
-		//~assert true == lic.LIC9(LIC9_X1, LIC9_Y1, 5, Math.PI/2.1, 1, 1) : "LIC9 (true) Failed";
-		//~double[] LIC9_X2 = {0, 0, 0, 0, 1.0};
-		//~double[] LIC9_Y2 = {0, 0, 1.0, 0, 1.0};
-		//~assert false == lic.LIC9(LIC9_X2, LIC9_Y2, 5, Math.PI/2, 1, 1) : "LIC9 (false) Failed";	
-
-		//LIC10
 		LIC10_test();
-		//~double[] LIC10_X1 = {0, 0, 1.0, 0, 2.0};
-		//~double[] LIC10_Y1 = {0, 0, 2.0, 0, 0};
-		//~assert true == lic.LIC10(LIC10_X1, LIC10_Y1, 5, 1.95, 1, 1) : "LIC10 (true) Failed";
-		//~double[] LIC10_X2 = {-2.0, 0, -1.0, 0, 0};
-		//~double[] LIC10_Y2 = {0, 0, -2.0, 0, 0};
-		//~assert false == lic.LIC10(LIC10_X2, LIC10_Y2, 5, 2.0, 1, 1) : "LIC10 (false) Failed";	
-		
-		//LIC11
 		LIC11_test();
-		//~double[] LIC11_X1 = {0, 1, 2, 3 ,-1, 0};
-		//~double[] LIC11_Y1 = {0, 0, -1, 0, 0,-1};
-		//~assert true == lic.LIC11(LIC11_X1, LIC11_Y1, 6, 3) : "LIC11 (true) Failed";
-		//~double[] LIC11_X2 = {0, 1, 2, 3 ,4, 5};
-		//~double[] LIC11_Y2 = {0, 0, -2, 0, 0, 5};
-		//~assert false == lic.LIC11(LIC11_X2, LIC11_Y2, 6, 1) : "LIC11 (false) Failed";		
-
-		//LIC12
 		LIC12_test();
-		//k_pts = 2, length1 = 5, length2 = 100
-		//~double[] LIC12_X1 = {0, 1, 0, 0, 10};
-		//~double[] LIC12_Y1 = {0, 0, 2, 0, 10};
-		//~assert true == lic.LIC12(LIC12_X1, LIC12_Y1, 5, 5, 100, 2) : "LIC12 (true) Failed";
-		//only greater than length1, but no two points closer than 100
-		//~double[] LIC12_X2 = {0, 100, 200, 300, 400};
-		//~double[] LIC12_Y2 = {0, 0, 0, 0, 0};
-		//~assert false == lic.LIC12(LIC12_X2, LIC12_Y2, 5, 5, 100, 2) : "LIC12 (false) Failed";
-		//only points closer than length2, but no two points further than 5
-		//~double[] LIC12_X3 = {0, 1, 2, 3, 4};
-		//~double[] LIC12_Y3 = {0, 0, 0, 0, 0};
-		//~assert false == lic.LIC12(LIC12_X3, LIC12_Y3, 5, 5, 100, 2) : "LIC12 (false) Failed";
-		
-		//LIC13
 		LIC13_test();
-		//a_pts = 1, b_pts = 1, radius1 = 10 radius2 = 100
-		//~double[] LIC13_X1 = {0, 1, 0, 0, 10};
-		//~double[] LIC13_Y1 = {0, 50, 2, 0, 50};
-		//~assert true == lic.LIC13(LIC13_X1, LIC13_Y1, 5, 10, 100, 1, 1) : "LIC13 (true) Failed";
-		//~double[] LIC13_X2 = {2000, 0, 1, 0, 0, 1000, 5000};
-		//~double[] LIC13_Y2 = {2000, 0, 50, 2, 0, 104, 5000};
-		//~assert false == lic.LIC13(LIC13_X2, LIC13_Y2, 7, 10, 100, 1, 1) : "LIC13 (false) Failed";
-		
-		//LIC14
 		LIC14_test();
-		//e_pts = 1, f_pts = 1, area1 = 5 area2 = 100
-		//~double[] LIC14_X1 = {0, 0, 4, 0, 0};
-		//~double[] LIC14_Y1 = {0, 0, 0, 0, 5}; //area 4*5/2 = 10
-		//~assert true == lic.LIC14(LIC14_X1, LIC14_Y1, 5, 5, 100, 1, 1) : "LIC14 (true) Failed";
-		//~double[] LIC14_X2 = {0, 0, 4, 0, 0};
-		//~double[] LIC14_Y2 = {0, 0, 0, 0, 500}; //area 4*500/2 = 1000
-		//~assert false == lic.LIC14(LIC14_X2, LIC14_Y2, 5, 5, 100, 1, 1) : "LIC14 (false) Failed";
-
+		
+		//Test of MatrixCalc methods
 
 		//CMV
+		MatrixCalc matrixcalc = new MatrixCalc();
+		
 		boolean[] answer = {true, true, true, true, true, true, false, true, true, false, true, true, true, true, true};
 		double[] CMV_X1 = {0, 1, 1, -1, 3};
 		double[] CMV_Y1 = {0, 0, -1, -1, -1};
@@ -369,7 +236,7 @@ class DecideTest {
 		n_pts = 3;
 		quads = 2;
 
-		boolean[] vector = decide.CMV(CMV_X1, CMV_Y1, numpoints, length1, length2, area1, area2,
+		boolean[] vector = matrixcalc.CMV(CMV_X1, CMV_Y1, numpoints, length1, length2, area1, area2,
 		radius1, radius2, epsilon, dist, a_pts, b_pts, c_pts, 
 		d_pts, e_pts, f_pts, g_pts, k_pts, n_pts, q_pts, quads);
 		for(int i = 0; i < 15; i++){
@@ -399,7 +266,7 @@ class DecideTest {
 			{"NOTUSED", "NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED","NOTUSED", "NOTUSED", "NOTUSED", "NOTUSED"}
 			};
 		
-		boolean[][] pum_mock = decide.PUM(cmv_mock, lcm_mock);
+		boolean[][] pum_mock = matrixcalc.PUM(cmv_mock, lcm_mock);
 		
 		//testing a handfull of values, using the three different operators and true and false cases
 		assert pum_mock[0][0] == true : " PUM[0][0] (true) failed";
@@ -428,7 +295,7 @@ class DecideTest {
 			{true, false, true, false, true, false, true, false, true, false, true, false, true, false, true}
 			};	
 		boolean[] puv_mock = {true, false, true, true, true, true, true, true, true, true, true, true, true, true, false};
-		boolean[] fuv_mock = decide.FUV(pum_mock2, puv_mock);
+		boolean[] fuv_mock = matrixcalc.FUV(pum_mock2, puv_mock);
 		boolean[] expected_fuv = {true, true, false, false, false, false, false, false, false, false, false, false, false, false, true };
 		for(int i = 0; i < 15; i++){
 			assert fuv_mock[i] == expected_fuv[i] : " FUV test failed";
